ccbe3afd189d373386e82d11bf9df2e70e7f5ccd
gooner <gooner@gooners-MacBook-Pro.local>
2018-07-12 16:29:22 +0800

    Modify my define config

--- demo02/config/config.go
+++ demo02/config/config.go
@@ -1,20 +1,20 @@
 package config
 
 import (
-	"log"
 	"strings"
 
 	"github.com/fsnotify/fsnotify"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 )
 
 type Config struct {
-	Name string
+	name string
 }
 
 func Init(cfg string) error {
 	c := Config{
-		Name: cfg,
+		name: cfg,
 	}
 
 	// 初始化配置文件
@@ -29,17 +29,20 @@ func Init(cfg string) error {
 }
 
 func (c *Config) initConfig() error {
-	if c.Name != "" {
-		viper.SetConfigFile(c.Name) // 如果指定了配置文件，则解析指定的配置文件
+	if c.name != "" {
+		viper.SetConfigFile(c.name) // 如果指定了配置文件，则解析指定的配置文件
 	} else {
 		viper.AddConfigPath("conf") // 如果没有指定配置文件，则解析默认的配置文件
 		viper.SetConfigName("config")
 	}
+
 	viper.SetConfigType("yaml")     // 设置配置文件格式为YAML
 	viper.AutomaticEnv()            // 读取匹配的环境变量
 	viper.SetEnvPrefix("APISERVER") // 读取环境变量的前缀为APISERVER
+
 	replacer := strings.NewReplacer(".", "_")
 	viper.SetEnvKeyReplacer(replacer)
+
 	if err := viper.ReadInConfig(); err != nil { // viper解析配置文件
 		return err
 	}
@@ -51,6 +54,6 @@ func (c *Config) initConfig() error {
 func (c *Config) watchConfig() {
 	viper.WatchConfig()
 	viper.OnConfigChange(func(e fsnotify.Event) {
-		log.Printf("Config file changed: %s", e.Name)
+		log.Infof("Config file changed: %s", e.Name)
 	})
 }
